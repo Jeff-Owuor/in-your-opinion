c345c3abbb36c33aeb848fdd86933bb1f75c4fb0
Jeff <Xavierjeff451@gmail.com>
2022-06-11 14:44:53 +0300

    login page

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import re_path,path
-from .views import register,index
+from .views import register,index,loginPage
 
 urlpatterns = [
     re_path(r'^$', index , name='home'),
     path('register/', register, name='register'),
+    path('login/', loginPage, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib.auth import logout,login,authenticate
 
 def register(request):
     form = RegisterForm()
@@ -11,4 +12,19 @@ def register(request):
     
     return render(request, 'registration/register.html', {"form":form})
 
-# Create your views here.
+
+def loginPage(request):
+    context ={}
+    
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+    
+    return render(request,'registration/login.html',context)
+
